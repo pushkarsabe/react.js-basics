0fb9757b9c25acf11a5d9e9c93c66a86b4d7c46f
pushkarsabe <pushkarsabe@gmail.com>
2024-09-22 16:48:34 +0530

    first commit

---
+++ Test project/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <>
+      <div>
+        <img
+          src="Dunki 2023 poster.jpg"
+          alt="Dunki 2023 poster.jpg"
+          width="40%"
+          height="40%"
+        />
+      </div>
+      <h2>Netflix</h2>
+      <p>Best shows : dunki</p>
+    </>
+  );
+};
